b58d5b4dbf3a33de1215cb3d1770019ede9e17c7
TAMJID AHMED <ahmed15-4895@diu.edu.bd>
2025-04-05 23:16:46 +0600

    initialized a express server

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Tamjid Ahmed")
+const express = require('express')
+require("dotenv").config()
+const app = express()
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/yr', (req, res) => {
+  res.send('Server Wokring')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
+})
