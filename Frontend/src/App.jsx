6f041b5208a4c4c7582e8269655c57a0c2336c2b
TAMJID AHMED <ahmed15-4895@diu.edu.bd>
2025-04-08 01:33:35 +0600

    fixed cors Issue

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import axios from 'axios'
+// http://localhost:5000/jokes
+function App() {
+  const [jokes, setJokes] = useState([])
+ 
+  useEffect(() => {
+    axios.get("/api/jokes")
+      .then((res) => {
+        setJokes(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1 className='text-white'>Jokes {jokes.length}</h1>
+     <div>
+      {
+        jokes.map((joke)=>
+        <div  key={joke.id}>
+  <h3 className='text-start'>{joke?.title}</h3>
+        </div>)
+      }
+     </div>
+
+
+     
+    </>
+  )
+}
+
+export default App
